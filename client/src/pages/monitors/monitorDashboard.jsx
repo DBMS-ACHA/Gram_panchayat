311359b567261d3f06fd8c525f28c2c90f0b79dd
Slum-dog <harshitiitkgp@kgpian.iitkgp.ac.in>
2025-03-01 15:33:06 +0530

    minor changes

--- client/src/pages/monitors/monitorDashboard.jsx
+++ client/src/pages/monitors/monitorDashboard.jsx
@@ -65,7 +65,7 @@ const MonitorDashboard = () => {
             navigate('/');
         } catch (error) {
             console.error('Logout failed:', error);
-            navigate('/login');
+            navigate('/');
         }
     };
 
